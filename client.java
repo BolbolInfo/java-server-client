b11f4af19950b4bdb89712fae641af5e9e41345f
BolbolInfo 1.0 <bilelabid4@gmail.com>
2025-03-07 22:23:39 +0100

    simple connection

--- client.java
+++ client.java
@@ -1,5 +1,10 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class client {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException{
+        Socket client = new Socket("localhost", 8080);
+        System.out.println("Connected to server on port 8080");
+        client.close();
     }
 }
--- server.java
+++ server.java
@@ -1,5 +1,16 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class server{
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException{
+        ServerSocket server = new ServerSocket(8080);
+        System.out.println("Server is running on port 8080");
+        while (true) {
+            try (Socket client = server.accept()) {
+                
+                System.out.println("Client connected");
+            }
+        }     
     }
 }
